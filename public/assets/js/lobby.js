873bb841e65c4e855c3b306874b8f6a1ad3f0741
sylvainbapst <sbapst.j@gmail.com>
2021-02-03 15:56:36 +0100

    placeholder select

--- public/assets/js/lobby.js
+++ public/assets/js/lobby.js
@@ -1,5 +1,5 @@
-window.onload = () => {
-    $(document).ready(function() {
-        $('.user-select').select2();
+$(document).ready(function() {
+    $('.user-select').select2({
+        placeholder: "Ecrivez le nom d'utilisateur"
     });
-}
+});
